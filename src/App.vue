d5ff7d0d35834400e4b76061595de0bf04a86739
Haze272 <studiorain27@gmail.com>
2022-10-08 14:58:12 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <HelloWorld msg="Рахим Итегез эченде Vue.js + TypeScript App"/>
 </template>
 
 <script lang="ts">
@@ -16,12 +15,19 @@ export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr;
 }
+
 </style>
